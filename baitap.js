829d5856f4d2c0b64748fe148645f064b2f83aaa
dung1993 <crorstar1704@gmail.com>
2022-08-09 17:23:16 +0700

    baitap2.html

--- baitap.js
+++ baitap.js
@@ -1,3 +1,5 @@
+// Điểm trung bình
+
 let inputPhysics = prompt("Physics")
 let inputChemistry = prompt("Chemistry")
 let inputBiological = prompt("Biological")
@@ -6,5 +8,13 @@ let Chemistry = parseInt(inputChemistry)
 let Biological = parseInt(inputBiological)
 let Average = (Physics + Chemistry + Biological)/3
 document.write ('Average =' + Average)
+
+// Đổi độ C sang độ F
+let inputC = prompt("C")
+let C = parseInt(inputC)
+let F = (C*9)/5-32
+document.write('F ='+ F)
+
+
 
  
