311815e07a5527af8912c75123806b797d50c73f
dung1993 <crorstar1704@gmail.com>
2022-08-09 20:34:19 +0700

    baitap4.html

--- baitap.js
+++ baitap.js
@@ -6,15 +6,22 @@ let inputBiological = prompt("Biological")
 let Physics = parseInt(inputPhysics)
 let Chemistry = parseInt(inputChemistry)
 let Biological = parseInt(inputBiological)
-let Average = (Physics + Chemistry + Biological)/3
-document.write ('Average =' + Average)
+let Average = (Physics + Chemistry + Biological) / 3
+document.write('Average =' + Average)
 
 // Đổi độ C sang độ F
 let inputC = prompt("C")
 let C = parseInt(inputC)
-let F = (C*9)/5-32
-document.write('F ='+ F)
+let F = (C * 9) / 5 - 32
+document.write('F =' + F)
+
+// Diện tích hình tròn
+let r = parseInt(prompt("bán kính"))
+let S = 3.14 * r * r
+document.write('S =' + S)
 
 
-
- 
+// Chu vi hình tròn
+let r = parseInt(prompt("bán kinh"))
+let C = 3.14 * 2 * r
+document.write('C =' + C)
