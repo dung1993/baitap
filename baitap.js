21e24f64870d3d3811f526c15ba5f40b9add5e45
dung1993 <crorstar1704@gmail.com>
2022-08-09 17:14:44 +0700

    baitap.html

---
+++ baitap.js
@@ -0,0 +1,10 @@
+let inputPhysics = prompt("Physics")
+let inputChemistry = prompt("Chemistry")
+let inputBiological = prompt("Biological")
+let Physics = parseInt(inputPhysics)
+let Chemistry = parseInt(inputChemistry)
+let Biological = parseInt(inputBiological)
+let Average = (Physics + Chemistry + Biological)/3
+document.write ('Average =' + Average)
+
+ 
