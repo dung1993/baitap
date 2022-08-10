7fe033a0b46e12d9364cd509ce3470f79c93d2b2
dung1993 <crorstar1704@gmail.com>
2022-08-10 11:29:05 +0700

    baitap21.html

---
+++ anhkhoa.js
@@ -0,0 +1,6 @@
+
+    let a = 5
+    let b = 7
+    let c = 10
+   
+    console.log(`${a}^2 + ${b}*2 = (${a}+${b})*(${a}-${b})`)
--- baitap21.js
+++ baitap21.js
@@ -1,4 +1,17 @@
 function showConvert(){
-    let Money = document.getElementById(money).value
-    
+    let Money = document.getElementById("money").value;
+    let Form = document.getElementById("formcurrency").value;
+    let To = document.getElementById("tocurrency").value;
+    let result;
+
+    if(Form == "VND" && To == "USD"){
+        result = "result: " + (Money / 23000) + "$";
+    }else if(Form == "USD" && To == "VND"){
+        result = "result:" + (Money * 23000) + "VND";
+    }else if(Form == "VND"){
+        result = "result: " + Money + "VND";
+    } else {
+        result = "result: " + Money + "$";
+    }
+    document.getElementById('result').innerHTML = result;
 }
